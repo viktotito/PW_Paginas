24de2ec0d39c9c7fdacb3a1f59d083e5923a838a
VICTORIA RIVAS SALAS <victoria.rivassls@uanl.edu.mx>
2021-04-01 10:43:28 -0600

    version 1

---
+++ pagina/js/principal.js
@@ -0,0 +1,18 @@
+var comment = [ 
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" },
+{"nombre": "Panchito Pistoles", "hora": "29 de marzo del 2021", "body":"contenido de la pregunta 1" }
+];
+
+
+
+
+for (var i=0; i<comment.length; i++){
+
+}
